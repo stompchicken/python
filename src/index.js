4aab5e99319947024825e2137a8dc46ea4487315
Stephen Spencer <steve@stompchicken.com>
2018-11-12 13:20:01 +0000

    Refactor game logic

--- src/index.js
+++ src/index.js
@@ -12,12 +12,7 @@ var config = {
 var keys;
 var game = new Phaser.Game(config);
 
-var programState = {
-    map: initMap(),
-    cursor: [0, 0],
-    snake: initSnake()
-
-}
+var programState = initProgramState()
 
 var gameState = {
     map: [],
@@ -53,47 +48,28 @@ function create() {
     updateGame(programState, gameState);
 }
 
-var ACTION_NONE = 0;
-var ACTION_STEP = 1;
-
-
 function update() {
 
     if (Phaser.Input.Keyboard.JustDown(keys.D)) {
-        gameState.cursorX = Math.min(gameState.cursorX + 1, MAP_WIDTH - 1);
+        gameState.cursorIdx = move(gameState.cursorIdx, Direction.EAST);
         updateGame(programState, gameState);
     } else if(Phaser.Input.Keyboard.JustDown(keys.A)) {
-        gameState.cursorX = Math.max(gameState.cursorX - 1, 0);
+        gameState.cursorIdx = move(gameState.cursorIdx, Direction.WEST);
         updateGame(programState, gameState);
     } else if(Phaser.Input.Keyboard.JustDown(keys.W)) {
-        gameState.cursorY = Math.max(gameState.cursorY - 1, 0);
+        gameState.cursorIdx = move(gameState.cursorIdx, Direction.NORTH);
         updateGame(programState, gameState);
     } else if(Phaser.Input.Keyboard.JustDown(keys.S)) {
-        gameState.cursorY = Math.min(gameState.cursorY + 1, MAP_HEIGHT - 1);
+        gameState.cursorIdx = move(gameState.cursorIdx, Direction.SOUTH);
+        updateGame(programState, gameState);
+    } else if(Phaser.Input.Keyboard.JustDown(keys.E)) {
+        programState = doAction(programState, Action.STEP_FORWARD);
         updateGame(programState, gameState);
     }
-
-    var action = mapKeys();
-
-    if (action != ACTION_NONE) {
-        programState = doAction(programState, action);
-        updateGame(programState, gameState);
-    }
-
-
 }
-
-function mapKeys() {
-    if (Phaser.Input.Keyboard.JustDown(keys.E)) {
-        return ACTION_STEP;
-    } else {
-        return ACTION_NONE;
-    }
-}
-
 
 function doAction(state, action) {
-    if (action == ACTION_STEP) {
+    if (action == Action.STEP_FORWARD) {
         return stepFwd(state);
     }
 }
@@ -121,58 +97,12 @@ function updateGame(programState, gameState) {
 
     // update snake
     updateSprite(gameState.snake[0], programState.snake.head, 3);
-
     for (var i = 0; i < programState.snake.body.length; i++) {
         updateSprite(gameState.snake[i+1], programState.snake.body[i], 2);
     }
-
     for(var i = programState.snake.body.length; i < MAX_SNAKE - (programState.snake.body.length + 1); i++) {
         updateSprite(gameState.snake[i+programState.snake.body.length + 1], programState.snake.body[i], 0);
     }
-}
 
-function initMap() {
-    var map = []
-    for(var i = 0; i < MAP_WIDTH * MAP_HEIGHT; i++) {
-        map.push(Cell.EMPTY);
-    }
-
-     // Initialise map
-    for(var i = 0; i < MAP_WIDTH; i++) {
-        map[coordsToIdx(i, 0)] = Cell.BLOCK;
-        map[coordsToIdx(i, MAP_HEIGHT - 1)] = Cell.BLOCK;
-    }
-
-    for(var i = 0; i < MAP_HEIGHT; i++) {
-        map[coordsToIdx(0, i)] = Cell.BLOCK;
-        map[coordsToIdx(MAP_WIDTH - 1, i)] = Cell.BLOCK;
-    }
-
-    return map;
-}
-
-var DIRECTION_EAST = 0;
-
-function initSnake() {
-    return {
-        head: coordsToIdx(2, 5),
-        body: [coordsToIdx(1, 5)],
-        direction: 0,
-    }
-}
-
-
-function stepFwd(state) {
-    headCoords = idxToCoords(state.snake.head)
-    if (state.snake.direction == 0) {
-        state.snake.head = coordsToIdx(headCoords[0] + 1, headCoords[1])
-    }
-
-    console.log(state.snake.head)
-
-    return state;
-}
-
-function editCell(state, x, y, cell) {
-    return state;
+    // if terminated, show message
 }
--- src/python.js
+++ src/python.js
@@ -7,6 +7,19 @@ var Cell = {
     BLOCK: 2
 }
 
+var Direction = {
+    NORTH: 0,
+    SOUTH: 1,
+    EAST: 2,
+    WEST: 3
+}
+
+var Action = {
+    NONE: 0,
+    STEP_FORWARD: 1,
+    EDIT_CELL: 2
+}
+
 function idxToCoords(index) {
     return [index % MAP_WIDTH, Math.floor(index / MAP_WIDTH)];
 }
@@ -15,10 +28,76 @@ function coordsToIdx(x, y) {
     return (y * MAP_WIDTH) + x;
 }
 
+function clamp(x, min, max) {
+  return Math.min(Math.max(x, min), max);
+}
+
 function translate(index, dx, dy) {
-    return 0;
+    var coords = idxToCoords(index);
+    coords[0] = clamp(coords[0] + dx, 0, MAP_WIDTH - 1);
+    coords[1] = clamp(coords[1] + dy, 0, MAP_HEIGHT - 1);
+    return coordsToIdx(coords[0], coords[1]);
 }
 
 function move(index, direction) {
-    return 0;
+    if(direction == Direction.NORTH) {
+        return translate(index, 0, -1);
+    } else if(direction == Direction.SOUTH) {
+        return translate(index, 0, +1);
+    } else if(direction == Direction.EAST) {
+        return translate(index, +1, 0);
+    } else if(direction == Direction.WEST) {
+        return translate(index, -1, 0);
+    } else {
+        console.error("Unknown direction: "+direction)
+    }
+}
+
+function initProgramState() {
+    return {
+        map: initMap(),
+        cursor: [0, 0],
+        snake: initSnake()
+    }
+}
+
+function initMap() {
+    var map = []
+    for(var i = 0; i < MAP_WIDTH * MAP_HEIGHT; i++) {
+        map.push(Cell.EMPTY);
+    }
+
+     // Initialise map
+    for(var i = 0; i < MAP_WIDTH; i++) {
+        map[coordsToIdx(i, 0)] = Cell.BLOCK;
+        map[coordsToIdx(i, MAP_HEIGHT - 1)] = Cell.BLOCK;
+    }
+
+    for(var i = 0; i < MAP_HEIGHT; i++) {
+        map[coordsToIdx(0, i)] = Cell.BLOCK;
+        map[coordsToIdx(MAP_WIDTH - 1, i)] = Cell.BLOCK;
+    }
+
+    return map;
+}
+
+function initSnake() {
+    return {
+        head: coordsToIdx(2, 5),
+        body: [coordsToIdx(1, 5)],
+        direction: Direction.EAST,
+        terminated: false
+    }
+}
+
+
+function stepFwd(state) {
+    state.snake.head = move(state.snake.head, state.snake.direction)
+
+    return state;
+}
+
+
+function editCell(state, x, y, cell) {
+    return state;
 }
